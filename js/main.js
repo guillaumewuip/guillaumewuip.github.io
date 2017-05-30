55aaedef6904f6ca2a7445fca7a0ea5d5252f210
Guillaume Wuip <guillaume@wuips.com>
2017-05-30 07:57:43 +0200

    Remove terminal

--- js/main.js
+++ js/main.js
@@ -1,30 +1,4 @@
 $(document).ready(function(){
-
-
-    $('.js-terminal').typist({});
-
-    $('.js-terminal').empty();
-
-    $('.js-terminal')
-        .typist('prompt', 'h1 class="term-line"')
-        .typist('type', "> ")
-        .typist('wait', 1000)
-        .typist('type', "Hi !")
-        .typist('prompt', 'h1 class="term-line"')
-        .typist('type', "> ")
-        .typist('wait', 1000)
-        .typist('type', "I'm a 21 year old student from France and a web developer")
-        .typist('prompt', 'h1 class="term-line"')
-        .typist('type', "> ")
-        .typist('wait', 1000)
-        .typist('type', "This summer ")
-        .typist('wait', 300)
-        .typist('type', "I'd like to be your intern !")
-        .typist('prompt', 'h1 class="term-line"')
-        .typist('type', "> ");
-        // .typist('hideCursor');
-
-
     $('.js-slideToTop').click(function(e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: 0 }, 'fast');
@@ -38,7 +12,5 @@ $(document).ready(function(){
         $('html, body').animate({
             scrollTop: $(target).offset().top
         }, 'fast');
-
     });
-
 });
